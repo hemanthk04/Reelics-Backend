6dd65728a1c4d0bb81f23a18e457f6fc2a4a74b4
hemanthk04 <hkapalavai@gmail.com>
2026-07-15 23:56:44 +0530

    Corrected the Public URL to send to the endpoint instead of S3 endpoint

--- src/main/java/com/projects/reelics/services/R2StorageService.java
+++ src/main/java/com/projects/reelics/services/R2StorageService.java
@@ -17,10 +17,12 @@ public class R2StorageService {
     private final S3Client s3;
     private final String bucket;
     private final String endpoint;
+    private final String publicBaseUrl;
 
     public R2StorageService() {
         this.bucket = System.getenv("R2_BUCKET_NAME");
         this.endpoint = System.getenv("R2_ENDPOINT");
+        this.publicBaseUrl = System.getenv("R2_PUBLIC_BASE_URL");
 
         AwsBasicCredentials creds = AwsBasicCredentials.create(
                 System.getenv("R2_ACCESS_KEY_ID"),
@@ -29,14 +31,13 @@ public class R2StorageService {
 
         this.s3 = S3Client.builder()
                 .endpointOverride(URI.create("https://" + endpoint))
-                .region(Region.US_EAST_1) // dummy, R2 ignores it
+                .region(Region.US_EAST_1) // Dummy region, R2 ignores it
                 .credentialsProvider(StaticCredentialsProvider.create(creds))
                 .serviceConfiguration(b -> b
                         .pathStyleAccessEnabled(true)
                         .chunkedEncodingEnabled(false)
                 )
                 .build();
-
     }
 
     public String upload(File file, String key) {
@@ -48,6 +49,10 @@ public class R2StorageService {
 
         s3.putObject(req, RequestBody.fromFile(file));
 
-        return "https://" + endpoint + "/" + bucket + "/" + key;
+        String baseUrl = publicBaseUrl.endsWith("/")
+                ? publicBaseUrl.substring(0, publicBaseUrl.length() - 1)
+                : publicBaseUrl;
+
+        return baseUrl + "/" + key;
     }
 }
